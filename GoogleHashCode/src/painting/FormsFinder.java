02d24ab734bc9d45cf3ac83173e668e0d2dd734d
lanevska <lanevska@anna-desktop>
2016-02-07 17:11:07 +0100

    find forms

---
+++ GoogleHashCode/src/painting/FormsFinder.java
@@ -0,0 +1,65 @@
+package painting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import dataCenter.Coord;
+
+public class FormsFinder {
+	
+	private List<String> grid;
+	private int rows;
+	private int cols;
+	
+	public FormsFinder(List<String> grid) {
+		this.grid = new ArrayList<String>(grid);
+	
+		this.grid.remove(0);
+		this.rows = this.grid.size();
+		this.cols = this.grid.get(0).length();
+	}
+	
+	
+	
+	public List<List<Coord>> findForms(){
+		List<List<Coord>> forms = new ArrayList<List<Coord>>();
+		boolean[][] visited = new boolean[rows][cols];
+		for(int r = 0; r < rows; r++){
+			for(int c = 0; c < cols; c++){
+				if (grid.get(r).charAt(c) == '#'){
+					List<Coord> form= new ArrayList<Coord>();
+					int size = findForms(r, c, visited, form);
+					if (size > 0){
+						forms.add(form);
+					}
+				}
+			}
+			
+		}
+		return forms;
+	}
+	
+	
+	private int findForms(int row, int col, boolean[][] visited, List<Coord> form ){
+		if (row < 0 || col < 0 || row >= rows || col >= cols ||	visited[row][col]){
+			return 0;
+		} else if ( grid.get(row).charAt(col) == '.'){
+			visited[row][col] = true;
+			return 0;
+		}
+		int size = 1; 
+		visited[row][col] = true;
+		form.add(new Coord(row, col));
+		
+		size += findForms(row+1, col, visited, form);
+		size += findForms(row-1, col, visited, form);
+		size += findForms(row, col-1, visited, form);
+		size += findForms(row, col+1, visited, form);
+		return size;
+				
+	}	
+	
+
+	
+}
--- GoogleHashCode/src/painting/Main.java
+++ GoogleHashCode/src/painting/Main.java
@@ -3,6 +3,8 @@ package painting;
 import java.util.ArrayList;
 import java.util.List;
 
+import dataCenter.Coord;
+
 public class Main {
 	
 	private static void writeSolution(List<String> solution) {
@@ -13,8 +15,7 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
-		List<String> grid = FileManager.read("input.in");
-		
+		List<String> grid = FileManager.read("input3.in");
 		Solution solution1 = new SolutionLineByLine(grid);
 		List<String> ans1 = solution1.solve();
 		Solution solution2 = new SolutionColumnByColumn(grid);
@@ -23,5 +24,18 @@ public class Main {
 		writeSolution(ans1.size() < ans2.size() ? ans1 : ans2);
 		
 		System.out.println((ans1.size() < ans2.size() ? "Ligne par ligne" : "Colonne par colonne") + " : " + ans1.size() + " / " + ans2.size());
+	
+		
+		FormsFinder ff = new FormsFinder(grid);
+		List<List<Coord>> forms = ff.findForms();
+		System.out.println("Number of Forms = "+forms.size());
+	/**	for(List<Coord> f : forms){
+			System.out.println(f.size());
+			for(Coord coord : f){
+				System.out.println(coord.getRow()+ " "+coord.getColumn());
+			}
+			System.out.println("===========");
+		}
+		*/
 	}
 }
